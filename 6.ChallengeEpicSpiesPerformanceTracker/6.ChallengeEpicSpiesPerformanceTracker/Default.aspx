146176d4fab70c6baa383150cbe1fbeeaaa73a82
Tonja Owen <tonjaowen@gmail.com>
2017-10-18 14:56:28 -0700

    summary

---
+++ 6.ChallengeEpicSpiesPerformanceTracker/6.ChallengeEpicSpiesPerformanceTracker/Default.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="_6.ChallengeEpicSpiesPerformanceTracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 150px;
+            height: 164px;
+        }
+        .auto-style2 {
+            font-family: "Agency FB";
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <h2>
+                <img alt="" class="auto-style1" src="epic-spies-logo.jpg" /><br />
+                <br />
+                <span class="auto-style2">Asset Performance Tracker</span></h2>
+            <p>
+                &nbsp;</p>
+            <p>
+                Asset Name:&nbsp;
+                <asp:TextBox ID="AssetTextBox" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                &nbsp;</p>
+            <p>
+                Elections Rigged:&nbsp;
+                <asp:TextBox ID="ElectionsTextBox" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                &nbsp;</p>
+            <p>
+                Acts of Subterfuge Performed:&nbsp;
+                <asp:TextBox ID="ActsTextBox" runat="server"></asp:TextBox>
+            </p>
+            <p>
+                &nbsp;</p>
+            <p>
+                <asp:Button ID="AddButton" runat="server" OnClick="AddButton_Click" style="height: 26px" Text="Add Asset" />
+            </p>
+            <p>
+                <asp:Label ID="ResultLabel" runat="server"></asp:Label>
+            </p>
+            <p>
+                &nbsp;</p>
+            <p>
+                &nbsp;</p>
+            <p>
+                &nbsp;</p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ 6.ChallengeEpicSpiesPerformanceTracker/6.ChallengeEpicSpiesPerformanceTracker/Default.aspx.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _6.ChallengeEpicSpiesPerformanceTracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        private readonly int newIndex;
+
+        public object ResultLabel { get; private set; }
+        public object Assets { get; private set; }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                string[] assets = new string[0];
+                int[] elections = new int[0];
+                int[] acts = new int[0];
+
+                ViewState.Add("Assets", assets);
+                ViewState.Add("Elections", elections);
+                ViewState.Add("Acts", acts);
+
+
+                /*protected void AddButton_Click(object sender, EventArgs e)
+                {
+
+                    }
+                }
+
+                protected void AddButton_Click(object sender, EventArgs e)
+                */
+                {
+
+
+                }
+
+            }
+        }
+
+        protected void AddButton_Click(object sender, EventArgs e)
+        {
+            string[] assets = (string[])ViewState["Assets"];
+            int[] elections = (int[])ViewState["Elections"];
+            int[] acts = (int[])ViewState["Acts"];
+
+            int NewLength = assets.Length + 1;
+
+            Array.Resize(ref assets, NewLength);
+            Array.Resize(ref elections, NewLength);
+            Array.Resize(ref acts, NewLength);
+
+            int NewIndex = assets.GetUpperBound(0);
+
+            assets[NewIndex] = AssetTextBox.Text;
+            elections[NewIndex] = int.Parse(ElectionsTextBox.Text);
+            acts[NewIndex] = int.Parse(ActsTextBox.Text);
+
+            ViewState["Assets"] = assets;
+            ViewState["Elections"] = elections;
+            ViewState["Acts"] = acts;
+
+            ResultLabel.Text = String.Format("Total Elections Rigged: {0}<br />Average Acts of Subterfuge per Asset: {1:N2}<br />(Last Asset you Added: {2})",
+               elections.Sum(),
+               acts.Average(),
+               assets [newIndex]);
+
+            AssetTextBox.Text = "";
+            ElectionsTextBox.Text = "";
+            ActsTextBox.Text = "";
+
+
+
+
+
+
+
+        }
+    }
+
+}
+
+
