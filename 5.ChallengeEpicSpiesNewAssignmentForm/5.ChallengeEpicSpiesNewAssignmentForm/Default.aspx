d9b02a5f8a49c45e49367acb8e058477dd90d0f2
Tonja Owen <tonjaowen@gmail.com>
2017-10-18 14:39:03 -0700

    summary

---
+++ 5.ChallengeEpicSpiesNewAssignmentForm/5.ChallengeEpicSpiesNewAssignmentForm/Default.aspx
@@ -0,0 +1,72 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="_5.ChallengeEpicSpiesNewAssignmentForm.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 190px;
+            height: 229px;
+        }
+        .auto-style2 {
+            font-family: "Agency FB";
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <img class="auto-style1" src="epic-spies-logo.jpg" /></div>
+        <h2 class="auto-style2">Spy New Assignment Form</h2>
+        <p class="auto-style2">
+            &nbsp;</p>
+        <p class="auto-style2">
+            Spy Code Name:&nbsp;
+            <asp:TextBox ID="CodeNameTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p class="auto-style2">
+            &nbsp;</p>
+        <p class="auto-style2">
+            New Assignment Name:&nbsp;
+            <asp:TextBox ID="NewAssignTextBox" runat="server"></asp:TextBox>
+        </p>
+        <p class="auto-style2">
+            &nbsp;</p>
+        <p class="auto-style2">
+            End Date of Previous Assignment</p>
+        <p class="auto-style2">
+            <asp:Calendar ID="PreviousCalendar" runat="server"></asp:Calendar>
+        </p>
+        <p class="auto-style2">
+            &nbsp;</p>
+        <p class="auto-style2">
+            Start Date of New Assignment</p>
+        <p class="auto-style2">
+            <asp:Calendar ID="NewCalendar" runat="server"></asp:Calendar>
+        </p>
+        <p class="auto-style2">
+            &nbsp;</p>
+        <p class="auto-style2">
+            Projected End Date of New Assignment</p>
+        <p class="auto-style2">
+            <asp:Calendar ID="EndCalendar" runat="server"></asp:Calendar>
+        </p>
+        <p class="auto-style2">
+            &nbsp;</p>
+        <p class="auto-style2">
+            <asp:Button ID="AssignButton" runat="server" OnClick="AssignButton_Click" Text="Assign Spy" />
+&nbsp;</p>
+        <p class="auto-style2">
+            &nbsp;</p>
+        <p class="auto-style2">
+            <asp:Label ID="ResultLabel" runat="server"></asp:Label>
+        </p>
+        <p class="auto-style2">
+            &nbsp;</p>
+        <p class="auto-style2">
+            &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ 5.ChallengeEpicSpiesNewAssignmentForm/5.ChallengeEpicSpiesNewAssignmentForm/Default.aspx.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _5.ChallengeEpicSpiesNewAssignmentForm
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                PreviousCalendar.SelectedDate = DateTime.Now.Date;
+                NewCalendar.SelectedDate = DateTime.Now.Date.AddDays(14);
+                EndCalendar.SelectedDate = DateTime.Now.Date.AddDays(21);
+
+            }
+
+            //protected void AssignButton_Click(object sender, EventArgs e)
+            //{
+                //Spies cost $500 per day
+                //TimeSpan TotalDurationAssignment = EndCalendar.SelectedDate.Subtract(NewCalendar.SelectedDate);
+                //double TotalCost = TotalDurationAssignment.TotalDays * 500.0;
+
+                //if > 21 days then add $1000
+               /* if (TotalDurationAssignment.TotalDays > 21)
+                {
+                    TotalCost += 1000.0;
+
+
+                }
+
+                ResultLabel.Text = String.Format("Assignment of {0} to assignment {1} is authorized. Budget total: {2:C}",
+                
+                        CodeNameTextBox.Text, 
+                        NewAssignTextBox.Text,
+                        TotalCost);
+
+                TimeSpan TimeBetweenAssignments = NewCalendar.SelectedDate.Subtract(PreviousCalendar.SelectedDate);
+                if (TimeBetweenAssignments.TotalDays < 14)
+                {
+                    ResultLabel.Text = "Error: Must allow atleast two weeks between " +
+                        "previous assignment and new assignment.";
+
+                    DateTime EarliestNewAssignementDate = PreviousCalendar.SelectedDate.AddDays(14);
+
+                    NewCalendar.SelectedDate = EarliestNewAssignementDate;
+                    NewCalendar.VisibleDate = EarliestNewAssignementDate;
+
+                }
+
+        }*/
+    }
+
+        protected void AssignButton_Click(object sender, EventArgs e)
+        {
+            //Spies cost $500 per day
+            TimeSpan TotalDurationAssignment = EndCalendar.SelectedDate.Subtract(NewCalendar.SelectedDate);
+            double TotalCost = TotalDurationAssignment.TotalDays * 500.0;
+
+            //if > 21 days then add $1000
+            if (TotalDurationAssignment.TotalDays > 21)
+            {
+                TotalCost += 1000.0;
+
+
+            }
+
+            ResultLabel.Text = String.Format("Assignment of {0} to assignment {1} is authorized. Budget total: {2:C}",
+
+                    CodeNameTextBox.Text,
+                    NewAssignTextBox.Text,
+                    TotalCost);
+
+            TimeSpan TimeBetweenAssignments = NewCalendar.SelectedDate.Subtract(PreviousCalendar.SelectedDate);
+            if (TimeBetweenAssignments.TotalDays < 14)
+            {
+                ResultLabel.Text = "Error: Must allow atleast two weeks between " +
+                    "previous assignment and new assignment.";
+
+                DateTime EarliestNewAssignementDate = PreviousCalendar.SelectedDate.AddDays(14);
+
+                NewCalendar.SelectedDate = EarliestNewAssignementDate;
+                NewCalendar.VisibleDate = EarliestNewAssignementDate;
+
+            }
+
+        }
+    }
+}
+    
